afc87f36fadfaa1cc8e63fd5eca80e9f922bf274
MV242667 <102163019+Cyberking23@users.noreply.github.com>
2024-08-06 09:10:56 -0600

    Modificacion
    Modificacion

--- JS/WeatherApp.js
+++ JS/WeatherApp.js
@@ -1,1 +1,9 @@
-const result
+const result = document.querySelector('result');
+const form = document.querySelector('get-weather')
+const nameCity = document.querySelector('#city');
+const nameCountry = document.querySelector('#country')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    console.log(nameCity.value)
+})
